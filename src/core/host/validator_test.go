49741a43d32a231bd2f2d72c7fecfc37dc947b64
Jirat Viriyataranon <jirat.viriyataranon@scbtechx.io>
2025-02-02 18:06:04 +0700

    test: adjust host validator test case

--- src/core/host/validator_test.go
+++ src/core/host/validator_test.go
@@ -1,89 +1,115 @@
 package host
 
 import (
-	"context"
 	"testing"
 )
 
-func TestRegisterHostRequestValid(t *testing.T) {
-	valid := func(problems map[string]string) bool {
-		return len(problems) == 0
+func TestValidateAuthority(t *testing.T) {
+	valid := func(err error) bool {
+		return err == nil
 	}
-	invalid := func(problems map[string]string) bool {
-		return len(problems) > 0
+	invalid := func(err error) bool {
+		return err != nil
 	}
 
 	tests := []struct {
-		name     string
-		request  registerhostRequest
-		expected func(map[string]string) bool
+		name      string
+		authority string
+		expected  func(err error) bool
 	}{
 		{
 			"Valid host with user info",
-			registerhostRequest{"alias": "https://user:password@www.example.com:443"},
+			"https://user:password@www.example.com:443",
 			valid,
 		},
 		{
 			"Valid host",
-			registerhostRequest{"alias": "https://www.example.com"},
+			"https://www.example.com",
 			valid,
 		},
 		{
 			"Valid host with port",
-			registerhostRequest{"alias": "http://localhost:8080"},
+			"http://localhost:8080",
 			valid,
 		},
 		{
 			"Valid ip address",
-			registerhostRequest{"alias": "ftp://192.168.0.1:21"},
+			"ftp://192.168.0.1:21",
 			valid,
 		},
 		{
 			"Invalid host empty scheme",
-			registerhostRequest{"alias": "invalid-url"},
+			"invalid-url",
 			invalid,
 		},
 		{
 			"Invalid host empty host",
-			registerhostRequest{"alias": "http://:8080"},
+			"http://:8080",
 			invalid,
 		},
 		{
 			"Invalid host empty protocol",
-			registerhostRequest{"alias": "://example.com"},
-			invalid,
-		},
-		{
-			"Valid alias",
-			registerhostRequest{"exampleAlias": "https://www.host.com"},
-			valid,
-		},
-		{
-			"Valid alias with -",
-			registerhostRequest{"example-alias": "https://www.host.com"},
-			valid,
-		},
-		{
-			"Invalid alias with /",
-			registerhostRequest{"example/alias": "https://www.host.com"},
-			invalid,
-		},
-		{
-			"Invalid alias with space",
-			registerhostRequest{"example alias": "https://www.host.com"},
-			invalid,
-		},
-		{
-			"Invalid alias with special character",
-			registerhostRequest{"example!alias": "https://www.host.com"},
+			"://example.com",
 			invalid,
 		},
 	}
 
 	for _, test := range tests {
-		result := test.request.valid(context.Background())
-		if !test.expected(result) {
-			t.Errorf("Test case failed: %v", test.name)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			result := validateAuthority(test.authority)
+			if !test.expected(result) {
+				t.Errorf("Test case failed: %v", test.name)
+			}
+		})
+	}
+}
+
+func TestValidateAlias(t *testing.T) {
+	valid := func(err error) bool {
+		return err == nil
+	}
+	invalid := func(err error) bool {
+		return err != nil
+	}
+
+	tests := []struct {
+		name     string
+		alias    string
+		expected func(err error) bool
+	}{
+		{
+			"Valid alias",
+			"exampleAlias",
+			valid,
+		},
+		{
+			"Valid alias with -",
+			"example-alias",
+			valid,
+		},
+		{
+			"Invalid alias with /",
+			"example/alias",
+			invalid,
+		},
+		{
+			"Invalid alias with space",
+			"example alias",
+			invalid,
+		},
+		{
+			"Invalid alias with special character",
+			"example!alias",
+			invalid,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := ValidateAlias(test.alias)
+			if !test.expected(result) {
+				t.Errorf("Test case failed: %v", test.name)
+			}
+		})
 	}
 }
